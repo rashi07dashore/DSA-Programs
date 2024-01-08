6464ebe6beeddbc6c72d0c3613537ff59af667b7
Rashi Dashore <dash.rashi07@gmail.com>
2024-01-08 16:21:01 +0530

    update

--- Stack and Queue/QueueMain.java
+++ Stack and Queue/QueueMain.java
@@ -2,7 +2,8 @@ class CustomQueue
 {
    protected int[] data;
     private static final int DEFAULT_SIZE = 10;
-    int end = -1;
+    int end = 0; //front will be 0th index always
+    //rear will be end pointer. pointing at empty space near last pointer
     public CustomQueue()
     {
         this(DEFAULT_SIZE);
@@ -12,15 +13,68 @@ class CustomQueue
         this.data = new int[size];
     }
     private boolean isFull() {
-       return end == data.length-1; //end is at last index i.e. Queue is full
+       return end == data.length; //end is at last index i.e. Queue is full
     }
     private boolean isEmpty(){
-        return end == -1;  //end is at initial position 
+        return end == 0;  //end is at initial position 
         //Queue is empty condition
+    }
+    public boolean insert(int item)
+    {
+       if(isFull())
+       {
+        return false;
+       }
+       data[end] = item;
+       end++; 
+       return true;
+    }
+    public int remove()
+    {
+        if(isEmpty()){
+            return -1;
+        }
+        int a = data[0];  //removing from front
+        for(int i=1; i<end; i++)
+        {
+            data[i-1] = data[i];
+        }
+        end--; //end pointer is decreased 
+        return a;
+    }
+    public void display(){
+        if(isEmpty())
+        return;
+        System.out.println("displaying data");
+        for(int i=0; i<end; i++)
+        {
+            System.out.print(data[i]+"->");
+        }
+        return;
+    }
+    public void displayFront_Rear()
+    {
+        if(isEmpty()){
+            System.out.println("Queue is empty, front and rear at same position");
+            return;
+        }
+        System.out.println("front is at index: 0 and rear(end) at index: "+end);
+        return;
     }
 }
 public class QueueMain {
     public static void main(String[] args) {
-    
+    CustomQueue queue= new CustomQueue();
+    queue.insert(90);
+    queue.insert(80);
+    queue.insert(53);
+    queue.insert(31);
+    queue.display();
+    System.out.println("Removed item: "+queue.remove());
+    queue.display();
+    queue.insert(10);
+    queue.display();
+    queue.displayFront_Rear();
+
     }
 }
