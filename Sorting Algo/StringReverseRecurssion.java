10faa05f9b52a62a520aa0bbf060bc186d0b2f8e
Rashi Dashore <dash.rashi07@gmail.com>
2024-05-28 02:54:08 +0530

    new

--- Sorting Algo/StringReverseRecurssion.java
+++ Sorting Algo/StringReverseRecurssion.java
@@ -13,10 +13,33 @@ public class StringReverseRecurssion {
         reverse(i+1, j-1, s);
 
     }
+    public static void palindrome(int i, int j, StringBuilder str)
+    {
+        //base case
+        if(i>j)
+        {
+            System.out.println("It is a palindrome");
+            return;
+        }
+
+        //false condition
+        if(str.charAt(i)!= str.charAt(j))
+        {
+        System.out.println("Not a palidrome");
+        return;
+        }
+        else{
+            //recursion
+            palindrome(i+1, j-1, str);
+        }
+
+    }
     public static void main(String[] args) {
         String s = "rashi";
         StringBuilder sb = new StringBuilder(s);
         reverse(0, s.length()-1 , sb);
         System.out.println(sb.toString());
+        StringBuilder str = new StringBuilder("RACECAR");
+        palindrome(0, str.length()-1, str);
     }
 }
